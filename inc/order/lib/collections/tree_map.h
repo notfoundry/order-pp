937648485e469fdc868ab4d21bade52e12dcb329
Mark Johnson <foundry@protonmail.com>
2020-11-09 08:26:00 -0500

    improve pattern-matching code used for treemap balancing

--- inc/order/lib/collections/tree_map.h
+++ inc/order/lib/collections/tree_map.h
@@ -10,6 +10,7 @@
 
 #define ORDER_PP_DEF_0tm_black ORDER_PP_CONST(0)
 #define ORDER_PP_DEF_0tm_red ORDER_PP_CONST(1)
+#define ORDER_PP_DEF_0tm_bblack ORDER_PP_CONST(1)
 
 #define ORDER_PP_DEF_0tm_tree \
 ORDER_PP_FN(8fn(8C /*Color*/, 8L /*LTree*/, 8N /*TreeNode*/, 8R /*RTree*/, \
@@ -48,29 +49,29 @@ ORDER_PP_FN(8fn(8E, 8K, 8T, \
 
 #define ORDER_PP_DEF_0tm_balance \
 ORDER_PP_FN(8fn(8T, \
-                8if(0tm_tree_colored(8T, 0tm_black), \
-                    8cond((8and(8isnt_nil(0tm_tree_left(8T)), 0tm_tree_colored(0tm_tree_left(8T), 0tm_red)), \
+                8cond((0tm_tree_colored(8T, 0tm_black), \
+                       8cond((8and(8isnt_nil(0tm_tree_left(8T)), 0tm_tree_colored(0tm_tree_left(8T), 0tm_red)), \
                            8cond((8and(8isnt_nil(0tm_tree_left(0tm_tree_left(8T))), 0tm_tree_colored(0tm_tree_left(0tm_tree_left(8T)), 0tm_red)), \
-                                  8apply(8fn(8_, 8T, 8Z, 8D, 8apply(8fn(8_, 8T, 8Y, 8C, 8apply(8fn(8_, 8A, 8X, 8B, \
+                                  8apply(8fn(8_, 8_, 8Z, 8D, 8_, 8_, 8Y, 8C, 8_, 8A, 8X, 8B, \
                                              0tm_tree(0tm_red, 0tm_tree(0tm_black, 8A, 8X, 8B), 8Y, 0tm_tree(0tm_black, 8C, 8Z, 8D))), \
-                                         8T)), 8T)), 8T)) \
+                                          8tuple_append(8T, 0tm_tree_left(8T), 0tm_tree_left(0tm_tree_left(8T))))) \
                                  (8and(8isnt_nil(0tm_tree_right(0tm_tree_left(8T))), 0tm_tree_colored(0tm_tree_right(0tm_tree_left(8T)), 0tm_red)), \
-                                  8apply(8fn(8_, 8T, 8Z, 8D, 8apply(8fn(8_, 8A, 8X, 8T, 8apply(8fn(8_, 8B, 8Y, 8C, \
+                                  8apply(8fn(8_, 8_, 8Z, 8D, 8_, 8A, 8X, 8_, 8_, 8B, 8Y, 8C, \
                                              0tm_tree(0tm_red, 0tm_tree(0tm_black, 8A, 8X, 8B), 8Y, 0tm_tree(0tm_black, 8C, 8Z, 8D))), \
-                                         8T)), 8T)), 8T)) \
+                                         8tuple_append(8T, 0tm_tree_left(8T), 0tm_tree_right(0tm_tree_left(8T))))) \
                                  (8else, 8T))) \
                           (8isnt_nil(0tm_tree_right(8T)), \
                            8cond((8and(8isnt_nil(0tm_tree_left(0tm_tree_right(8T))), 0tm_tree_colored(0tm_tree_left(0tm_tree_right(8T)), 0tm_red)), \
-                                  8apply(8fn(8_, 8A, 8X, 8T, 8apply(8fn(8_, 8T, 8Z, 8D, 8apply(8fn(8_, 8B, 8Y, 8C, \
+                                  8apply(8fn(8_, 8A, 8X, 8_, 8_, 8_, 8Z, 8D, 8_, 8B, 8Y, 8C, \
                                              0tm_tree(0tm_red, 0tm_tree(0tm_black, 8A, 8X, 8B), 8Y, 0tm_tree(0tm_black, 8C, 8Z, 8D))), \
-                                         8T)), 8T)), 8T)) \
+                                         8tuple_append(8T, 0tm_tree_right(8T), 0tm_tree_left(0tm_tree_right(8T))))) \
                                  (8and(8isnt_nil(0tm_tree_right(0tm_tree_right(8T))), 0tm_tree_colored(0tm_tree_right(0tm_tree_right(8T)), 0tm_red)), \
-                                  8apply(8fn(8_, 8A, 8X, 8T, 8apply(8fn(8_, 8B, 8Y, 8T, 8apply(8fn(8_, 8C, 8Z, 8D, \
+                                  8apply(8fn(8_, 8A, 8X, 8_, 8_, 8B, 8Y, 8_, 8_, 8C, 8Z, 8D, \
                                              0tm_tree(0tm_red, 0tm_tree(0tm_black, 8A, 8X, 8B), 8Y, 0tm_tree(0tm_black, 8C, 8Z, 8D))), \
-                                         8T)), 8T)), 8T)) \
+                                         8tuple_append(8T, 0tm_tree_right(8T), 0tm_tree_right(0tm_tree_right(8T))))) \
                                  (8else, 8T))) \
-                          (8else, 8T)), \
-                    8T)))
+                          (8else, 8T))) \
+                      (8else, 8T))))
 
 
 #define ORDER_PP_DEF_0tm_insert_impl \
