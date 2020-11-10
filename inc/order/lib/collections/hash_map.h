88375807ff264303622d6b6f890b8d22c39878a6
Mark Johnson <foundry@protonmail.com>
2020-11-10 12:40:03 -0500

    add file for experimental hashmap implementation

---
+++ inc/order/lib/collections/hash_map.h
@@ -0,0 +1,11 @@
+#ifndef ORDER_INC_ORDER_LIB_COLLECTIONS_HASH_MAP_H
+#define ORDER_INC_ORDER_LIB_COLLECTIONS_HASH_MAP_H
+
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE.)
+
+// First-order
+
+
+
+#endif
